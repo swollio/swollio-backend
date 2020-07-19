edafd102ba6a75c80a96693594a8d9b7387172c4
Thomas Barrett <thomasbarrett@Thomass-MacBook-Pro.local>
2020-07-19 12:48:29 -0400

    setup jest testing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,8 @@
 import app from './index'
 import supertest from 'supertest'
+const request = supertest(app)
 
-test("GET /", done => {
-  supertest(app)
-    .get("/")
-    .expect(200, 'hello world')
-    .end(done)
+it('Gets the test endpoint', async done => {
+  const res = await request.get('/test')
+  done()
 })
