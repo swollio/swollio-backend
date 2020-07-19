35a2821e7adf44330952ac2f9384a56f2a5bee86
Anshul Kamath <anshulkam@gmail.com>
2020-07-19 12:45:23 -0400

    Added the exercise route

---
+++ src/routes/exercise.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.status(500)
+    res.send("unimplemented")
+});
+
+router.get('/:id', (req, res) => {
+    res.status(500)
+    res.send("unimplemented")
+});
+
+router.post('/', (req, res) => {
+    res.status(500)
+    res.send("unimplemented")
+});
+
+export default router
