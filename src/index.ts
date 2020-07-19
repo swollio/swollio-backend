a5629983f0280eb98e567b0048b09949274663da
Thomas Barrett <thomasbarrett@Thomass-MacBook-Pro.local>
2020-07-19 12:18:25 -0400

    added team route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 
 import athleteRouter from './routes/athlete'
+import teamRouter from './routes/team'
 
 const app = express();
 
 app.use('/athlete', athleteRouter);
+app.use('/teams', teamRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, world!').end();
