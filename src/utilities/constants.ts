d883d72f9431bd84ad6d6912449a77ba5b175ab1
Anshul Kamath <anshulkam@gmail.com>
2020-07-21 23:31:18 -0400

    Got rid of old stuff

--- src/utilities/constants.ts
+++
@@ -1,6 +0,0 @@
-export const ATHLETE_D_TYPES = {
-    number: ["height", "weight", "age"],
-    string: ["firstName", "lastName", "gender"] 
-};
-
-export interface LooseType { [key: string] : string }
