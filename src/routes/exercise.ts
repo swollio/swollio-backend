74a70734549da441f4ad2ace1b43df13b7774a2d
Anshul Kamath <anshulkam@gmail.com>
2020-07-25 23:54:52 -0400

    Added documentation and more routes

--- src/routes/exercise.ts
+++ src/routes/exercise.ts
@@ -4,16 +4,19 @@ import { requirePermission } from '../middleware/auth';
 const router = express.Router()
 router.use(requirePermission([]))
 
+// Get exercises
 router.get('/', (req, res) => {
     res.status(500)
     res.send("unimplemented")
 });
 
+/// Find an exercise
 router.get('/:id', (req, res) => {
     res.status(500)
     res.send("unimplemented")
 });
 
+// Create an exercise
 router.post('/', (req, res) => {
     res.status(500)
     res.send("unimplemented")
--- src/routes/team.ts
+++ src/routes/team.ts
@@ -4,52 +4,62 @@ import { requirePermission } from '../middleware/auth';
 const router = express.Router()
 router.use(requirePermission([]))
 
+// List all teams
 router.get('/', (req, res) => {
     res.status(500)
     res.send('unimplemented')
 });
 
+// Find a team
 router.get('/:team_id', (req, res) => {
     res.status(500)
     res.send('unimplemented')
 });
 
+// List team's athletes
 router.get('/:team_id/athletes', (req, res) => {
     res.status(500)
     res.send('unimplemented')
 });
 
+// Add an athlete for a team
 router.post('/:team_id/athletes', (req, res) => {
     res.status(500)
     res.send('unimplemented')
 });
 
+// Delete athlete
 router.delete('/:team_id/athletes/:athlete_id', (req, res) => {
     res.status(500)
     res.send('unimplemented')
 });
 
+// Get a team's workouts 
 router.get('/:team_id/workouts', (req, res) => {
     res.status(500)
     res.send('unimplemented')
 });
 
+// Create a team workouts
 router.post('/:team_id/workouts', (req, res) => {
     res.status(500)
     res.send('unimplemented')
 });
 
+// Find a team's workout
 router.get('/:team_id/workouts/:workout_id', (req, res) => {
     res.status(500)
     res.send('unimplemented')
 });
 
+// Delete a team's workout
 router.delete('/:team_id/workouts/:workout_id', (req, res) => {
     res.status(500)
     res.send('unimplemented')
 });
 
-router.post('/:team_id/statistics', (req, res) => {
+// Get staistics to the team
+router.get('/:team_id/statistics', (req, res) => {
     res.status(500)
     res.send('unimplemented')
 });
