641f5c1eab3f4e62c8dc2bc0cadcfca511c5fbdb
Thomas Barrett <thomasbarrett@Thomass-MacBook-Pro.local>
2020-07-25 21:50:50 -0400

    Merge

--- sql/setup/custom_assignments_workouts.sql
+++ sql/setup/custom_assignments_workouts.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS assignments_workouts (
+CREATE TABLE IF NOT EXISTS custom_assignments_workouts (
     custom_assignment_id INT NOT NULL REFERENCES custom_assignments(id)  ON DELETE CASCADE,
     workout_id INT NOT NULL REFERENCES workouts(id)  ON DELETE CASCADE
 );
