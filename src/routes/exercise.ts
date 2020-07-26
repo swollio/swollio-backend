848353c28e2b56221557e5762e247e589c1f9966
Thomas Barrett <thomasbarrett@Thomass-MacBook-Pro.local>
2020-07-26 01:02:28 -0400

    minor mock data change

--- src/routes/exercise.ts
+++ src/routes/exercise.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { requirePermission } from '../middleware/auth';
+import db from '../utilities/database';
 
 const router = express.Router()
-router.use(requirePermission([]))
+// router.use(requirePermission([]))
 
 // Get exercises
 router.get('/', (req, res) => {
@@ -14,6 +15,15 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     res.status(500)
     res.send("unimplemented")
+});
+
+router.get('/:id/similar', (req, res) => {
+    db['exercises.filter_by_similar']([req.params.id])
+    .then((result) => res.send(result.rows))
+    .catch((error) => {
+        console.log(error)
+        res.status(500).send();
+    });
 });
 
 // Create an exercise
